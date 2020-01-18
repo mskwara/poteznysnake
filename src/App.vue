70d4bb440646e1880b93f3ba628096662fe4d2cd
Michał Skwara <michalon2000@gmail.com>
2020-01-18 22:09:08 +0100

    Collisions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <p>{{snake}}</p>
     <div id="map" v-bind:style="setMap()">
       <div class="snake_part" :key="part.id" v-for="part in snake" v-bind:style="partPosition(part)"></div>
     </div>
@@ -18,14 +17,16 @@ export default {
         {id: 1, x: 5, y: 6},
         {id: 2, x: 4, y: 6},
         {id: 3, x: 3, y: 6},
+        {id: 4, x: 2, y: 6},
       ],
       alive: true,
       nextId: 1,
       direction: "up",
-      snakeLength: 4,
-      speed: 300,
-      mapSize: 10,
+      snakeLength: 5,
+      speed: 250,
+      mapSize: 15,
       alreadyTurned: false,
+      nextMoves: [],
     }
   },
   mounted: function(){
@@ -58,44 +59,71 @@ export default {
     update(){
       if(this.alive){
         this.move();
-        if(this.snake[0].x == -1 || this.snake[0].x == this.mapSize || this.snake[0].y == -1 || this.snake[0].y == this.mapSize){
+        if(this.snake[0].x == -1 || this.snake[0].x == this.mapSize || this.snake[0].y == -1 || this.snake[0].y == this.mapSize){ //za mapą
           this.alive = false;
+          this.gameOver();
+        }
+        if(this.alive){
+          for(var i = this.snakeLength-1 ; i > 0 ; i--){      //ruch całego ciała
+            if(Object(this.snake[i]).y == this.snake[0].y && Object(this.snake[i]).x == this.snake[0].x){
+              this.alive = false;
+              this.gameOver();
+              break;
+            }
+          }
         }
         setTimeout(this.update, this.speed);
       }
       else {
-        alert("GAME OVER");
         this.reset();
       }
-      this.alreadyTurned = false;
+    },
+    gameOver(){
+      alert("GAME OVER");
     },
     move(){
-      for(var i = this.snakeLength ; i > 0 ; i--){      //ruch całego ciała
+      for(var i = this.snakeLength-1 ; i > 0 ; i--){      //ruch całego ciała
         Object(this.snake[i]).y = this.snake[i-1].y;
         Object(this.snake[i]).x = this.snake[i-1].x;
       }
-
-      if(this.direction == "up"){     //ruchy głowy
-        this.snake[0].y--;
+      if(this.nextMoves.length != 0){
+        if(this.nextMoves[0] == "up"){     //ruchy głowy
+          this.snake[0].y--;
+        }
+        else if(this.nextMoves[0] == "right"){
+          this.snake[0].x++;
+        }
+        else if(this.nextMoves[0] == "down"){
+          this.snake[0].y++;
+        }
+        else if(this.nextMoves[0] == "left"){
+          this.snake[0].x--;
+        }
+        this.nextMoves.splice(0,1);
       }
-      else if(this.direction == "right"){
-        this.snake[0].x++;
-      }
-      else if(this.direction == "down"){
-        this.snake[0].y++;
-      }
-      else if(this.direction == "left"){
-        this.snake[0].x--;
+      else {
+        if(this.direction == "up"){     //ruchy głowy
+          this.snake[0].y--;
+        }
+        else if(this.direction == "right"){
+          this.snake[0].x++;
+        }
+        else if(this.direction == "down"){
+          this.snake[0].y++;
+        }
+        else if(this.direction   == "left"){
+          this.snake[0].x--;
+        }
       }
     },
     turn(dir){
-      if(!this.alreadyTurned){
-        if(this.direction == "up" && dir == "down") return;
-        if(this.direction == "right" && dir == "left") return;
-        if(this.direction == "down" && dir == "up") return;
-        if(this.direction == "left" && dir == "right") return;
+      if(this.direction == "up" && dir == "down") return;
+      if(this.direction == "right" && dir == "left") return;
+      if(this.direction == "down" && dir == "up") return;
+      if(this.direction == "left" && dir == "right") return;
+      if(this.nextMoves.length < 2){
         this.direction = dir;
-        this.alreadyTurned = true;
+        this.nextMoves.push(dir);
       }
     },
     reset(){
@@ -106,7 +134,9 @@ export default {
         {id: 1, x: 5, y: 6},
         {id: 2, x: 4, y: 6},
         {id: 3, x: 3, y: 6},
+        {id: 4, x: 2, y: 6},
       ];
+      this.snakeLength = 5;
       this.update();
     }
   }
