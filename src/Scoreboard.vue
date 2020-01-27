100e8525c147fec2e67e1a90d23870e5b2ab150d
Michał Skwara <michalon2000@gmail.com>
2020-01-27 16:17:42 +0100

    top10, save

---
+++ src/Scoreboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <md-table v-model="list" md-sort="score" md-sort-order="asc" md-card>
+      <md-table-toolbar>
+        <h1 class="md-title">TOP 10</h1>
+      </md-table-toolbar>
+
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="Score" md-sort-by="score">{{ item.score }}</md-table-cell>
+        <md-table-cell md-label="Mode" md-sort-by="score" style="text-transform: uppercase">{{ item.mode }}</md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['list'],
+    methods: {
+
+    }
+  };
+</script>
+
+<style>
+.md-table-cell {
+  text-align: left;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import VueResource from "vue-resource";
+
+
+Vue.use(VueResource);
+// Vue.http.options.root = '/api';
+
 
 Vue.use(VueMaterial)
 
